800851d3282a240d0e4960685babc9a2038e4dc1
Karafizi Arthur <karafizi.artur@gmail.com>
2016-08-21 19:16:01 +0300

    Complete creating bitcoin watcher

--- app.js
+++ app.js
@@ -7,19 +7,6 @@ var port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', function (socket) {
-  console.log('new connection made');
-
-  socket.emit('message-from-server', {
-    greeting: 'Hello from Server'
-  });
-
-  socket.on('message-from-client', function (msg) {
-    console.log(msg);
-  });
-
-});
-
 server.listen(port, function () {
   console.log('Listening on port:', port);
 });
